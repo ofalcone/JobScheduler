ba3e139bd8137ab314f1248e5df2568e821d3211
Orlando Falcone 20011705 <20011705@studenti.uniupo.it>
2020-05-10 16:19:56 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JobScheduler.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
