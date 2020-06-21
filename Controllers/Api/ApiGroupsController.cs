e59f4416771a3e4d26dc797f39d502e739df68bd
Stefano Furlan <20010807@studenti.uniupo.it>
2020-06-21 22:26:51 +0200

    Added ApiGroupsController

---
+++ Controllers/Api/ApiGroupsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JobScheduler.Models;
+using System.Threading.Tasks;
+using JobScheduler.Abstract;
+using JobScheduler.Data;
+using JobScheduler.Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobScheduler.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = Constants.ADMIN_ROLE)]
+    public class ApiGroupsController : CrudController<JobSchedulerContext, Group>
+    {
+        public ApiGroupsController(JobSchedulerContext context) : base(context)
+        {
+        }
+    }
+
+}
