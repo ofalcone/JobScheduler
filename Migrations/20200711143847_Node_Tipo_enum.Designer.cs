1733ab5f4f2394ea6ef8d115f78ec35382190f8c
Stefano Furlan <20010807@studenti.uniupo.it>
2020-07-11 16:45:30 +0200

    To fix DB fields

--- Migrations/20200711143847_Node_Tipo_enum.Designer.cs
+++ Migrations/20200711143847_Node_Tipo_enum.Designer.cs
@@ -4,14 +4,16 @@ using JobScheduler.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace JobScheduler.Migrations
 {
     [DbContext(typeof(JobSchedulerContext))]
-    partial class JobSchedulerContextModelSnapshot : ModelSnapshot
+    [Migration("20200711143847_Node_Tipo_enum")]
+    partial class Node_Tipo_enum
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20200711143847_Node_Tipo_enum.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace JobScheduler.Migrations
+{
+    public partial class Node_Tipo_enum : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Tipo",
+                table: "Node",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Tipo",
+                table: "Node",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+    }
+}
